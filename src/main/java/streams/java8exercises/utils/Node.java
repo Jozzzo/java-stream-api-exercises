e83e7a158f771bf69d1e1a9cf88afc4850767b88
Artsiom <kop6en@gmail.com>
2023-03-22 13:04:25 +0100

    add exercises

---
+++ src/main/java/streams/java8exercises/utils/Node.java
@@ -0,0 +1,16 @@
+package streams.java8exercises.utils;
+
+@SuppressWarnings("unused")
+public class Node {
+
+    private final String name;
+
+    public Node(String name) {
+        this.name = name;
+    }
+
+    public String name() {
+        return name;
+    }
+
+}
